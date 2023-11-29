118bbcfc752ab01e50ae891e06d50a4d363f5394
egirard <egirad@aprogsys.com>
2023-11-29 14:44:03 +0100

    config - add db to docker-compose

--- app/app.js
+++ app/app.js
@@ -1,40 +1,20 @@
 // Importations nécessaires
-import express from 'express';
-import bodyParser from 'body-parser';
-import sequelize from './database/db.js'; 
-import userRoutes from './routes/UserRoutes.js';
-import setupAssociations from './models/associations.js';
+import express from "express";
+import bodyParser from "body-parser";
 
-setupAssociations();
-
-sequelize.sync()
-    .then(() => {
-        console.log('Modèles synchronisés avec la base de données');
-    })
-    .catch((error) => {
-        console.error('Erreur de synchronisation', error);
-    });
 const app = express();
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/api', userRoutes);
-
+const router = express.Router();
+router.get("/", (req, res) => {
+  res.send("hello");
+});
+app.use("/", router);
 
 // Authentification à la base de données
-sequelize.authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
-
-
 
 // Démarrage du serveur sur le port 3000
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });

87de6ab924cbb1697e2d68a02dbfef129fecb645
elenagir <elena.girard@gmail.com>
2023-11-30 14:54:57 +0100

    configs -migrations table user

---
+++ app/migrations/20231130122128-create-table-user.cjs
@@ -0,0 +1,38 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("user", {
+      id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+        allowNull: false,
+      },
+      first_name: { type: Sequelize.STRING, allowNull: false },
+      last_name: { type: Sequelize.STRING, allowNull: false },
+      email: { type: Sequelize.STRING, unique: true, allowNull: false },
+      password: { type: Sequelize.STRING },
+      birth_day: Sequelize.DATE,
+      password: { type: Sequelize.STRING },
+      phone: { type: Sequelize.STRING },
+      pseudo: { type: Sequelize.STRING },
+      gender: { type: Sequelize.STRING },
+      avatar: { type: Sequelize.STRING },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("user");
+  },
+};

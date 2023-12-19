83dbe8658ac428b3fd765c1fc9b1031465389770
egirard <egirad@aprogsys.com>
2023-12-19 10:06:28 +0100

    config - update models user and objective
    Co-authored-by: damirdine <damirdine@users.noreply.github.com>
    Co-authored-by: Dorian-Albiges <Dorian-Albiges@users.noreply.github.com>

--- app/models/user.js
+++ app/models/user.js
@@ -1,44 +1,76 @@
 export default (sequelize, Sequelize) => {
   const { DataTypes } = Sequelize;
-  const user = sequelize.define(
-    "user",
-    {
-      id: {
-        type: DataTypes.INTEGER.UNSIGNED,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      createdAt: {
-        field: "created_at",
-        type: Sequelize.DataTypes.DATE,
-        defaultValue: Sequelize.fn("NOW"),
-        allowNull: false,
-      },
-      updatedAt: {
-        field: "updated_at",
-        type: Sequelize.DataTypes.DATE,
-        defaultValue: Sequelize.fn("NOW"),
-        allowNull: false,
-      },
-      firstname: { type: DataTypes.STRING, allowNull: false },
-      lastname: { type: DataTypes.STRING, allowNull: false },
-      email: { type: DataTypes.STRING, unique: true, allowNull: false },
-      password: { type: DataTypes.STRING },
-      birthday: { type: DataTypes.DATE },
-      phone: { type: Sequelize.STRING },
-      pseudo: { type: DataTypes.STRING },
-      gender: { type: DataTypes.STRING },
-      avatar: { type: DataTypes.STRING },
+  const user = sequelize.define("user", {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-      tableName: "user",
-    }
-  );
+    created_at: {
+      field: "created_at",
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.fn("NOW"),
+      allowNull: false,
+    },
+    updated_at: {
+      field: "updated_at",
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.fn("NOW"),
+      allowNull: false,
+    },
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birthday: {
+      type: DataTypes.DATE,
+    },
+    phone: {
+      type: DataTypes.STRING,
+    },
+    pseudo: {
+      type: DataTypes.STRING,
+    },
+    gender: {
+      type: DataTypes.STRING,
+    },
+    avatar: {
+      type: DataTypes.STRING,
+    },
+    colocation_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      references: {
+        model: "colocation",
+        key: "id",
+      },
+    },
+  }, {
+    tableName: "user",
+  });
+
   user.associate = (models) => {
     user.belongsToMany(models.colocation, { through: "user_colocation" });
     user.hasOne(models.colocation, {
       foreignKey: "admin_user_id",
       as: "admin_user",
+    });
+    user.belongsToMany(models.objective, {
+      through: 'user_objective',
+      as: 'objectives',
+      foreignKey: 'user_id'
     });
   };
 

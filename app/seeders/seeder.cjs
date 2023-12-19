7f7751cf5dc77a2fb4f8fe11aa67bca1828ddb4a
egirard <egirad@aprogsys.com>
2023-12-19 10:41:31 +0100

    config -add seeder

--- app/seeders/seeder.cjs
+++ app/seeders/seeder.cjs
@@ -7,11 +7,13 @@ module.exports = {
       "colocation",
       [
         {
+          id: 1,
           name: "Colocation 1",
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
+          id: 2,
           name: "Colocation 2",
           created_at: new Date(),
           updated_at: new Date(),
@@ -24,6 +26,7 @@ module.exports = {
     const users = [];
     for (let i = 0; i < 10; i++) {
       users.push({
+        id: i + 1,
         firstname: `User${i + 1}`,
         lastname: `Lastname${i + 1}`,
         email: `user${i + 1}@example.com`,
@@ -53,10 +56,10 @@ module.exports = {
     const objectives = [];
     for (let i = 0; i < 10; i++) {
       objectives.push({
+        id: i + 1,
         name: `Objective ${i + 1}`,
         created_at: new Date(),
         updated_at: new Date(),
-        users_assigned: "",
         colocation_id: (i % 2) + 1,
         created_by: (i % 10) + 1,
       });
@@ -67,29 +70,42 @@ module.exports = {
     // Insérer des données pour les tableaux 'outgoing' et 'task'
     const outgoings = [];
     const tasks = [];
+    const user_objectives = [];
     for (let i = 0; i < 5; i++) {
       outgoings.push({
+        id: i + 1,
         final_expense: Math.random() * 100,
         created_at: new Date(),
         updated_at: new Date(),
+        objective_id: i + 1,
+      });
+      user_objectives.push({
+        user_id: (i % 2) + 1,
         objective_id: i + 1,
       });
     }
 
     for (let i = 5; i < 10; i++) {
       tasks.push({
+        id: i - 4,
         estimated_duration: Math.floor(Math.random() * 5) + 1,
         created_at: new Date(),
         updated_at: new Date(),
+        objective_id: i + 1,
+      });
+      user_objectives.push({
+        user_id: (i % 2) + 1,
         objective_id: i + 1,
       });
     }
 
     await queryInterface.bulkInsert("outgoing", outgoings);
     await queryInterface.bulkInsert("task", tasks);
+    await queryInterface.bulkInsert("user_objective", user_objectives);
   },
 
   down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("user_objective", null, {});
     await queryInterface.bulkDelete("task", null, {});
     await queryInterface.bulkDelete("outgoing", null, {});
     await queryInterface.bulkDelete("objective", null, {});

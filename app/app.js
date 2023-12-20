aebe6856b7cdbef062887d86d7a007371f876949
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2023-12-20 13:25:27 +0100

    refactor - clean app.js file remove hello route

--- app/app.js
+++ app/app.js
@@ -4,7 +4,6 @@ import morgan from "morgan";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-import models from "./models/index.js";
 import apiV1Router from "./routes/router.js";
 
 const app = express();
@@ -13,29 +12,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // http request logger
 app.use(morgan("tiny"));
-const router = express.Router();
-
-router.get("/hello", async (req, res) => {
-  try {
-    const data = await models.colocation.findAll({
-      include: [
-        "users",
-        {
-          model: models.objective,
-          as: "objectives",
-          // include: ["task", "outgoing"],
-        },
-        "admin_user",
-      ],
-    });
-
-    res.send({ data: data });
-  } catch (error) {
-    console.log(error);
-    res.send({ error });
-  }
-});
-app.use("/", router);
 
 app.use("/api/v1", apiV1Router);
 

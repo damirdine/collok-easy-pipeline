4c9b112c62f8d4f9f1d048169609f492f44631b9
Dorian <dorian.albiges@gmail.com>
2023-12-20 13:17:54 +0100

    Mise a jour des fonctions dans le controller

--- app/controllers/colocationController.js
+++ app/controllers/colocationController.js
@@ -3,20 +3,25 @@ import models from "../models/index.js";
 
 export const getColocations = async (req, res) => {
   try {
-    const data = await models.colocation.findAll({
-      include: [
-        "users",
-        // {
-        //   model: models.objective,
-        //   as: "objectives",
-        //   // include: ["task"],
-        // },
-        "admin_user",
-      ],
-    });
+    const data = await models.colocation.findAll();
     res.json({ data });
   } catch (error) {
     console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getColocationById = async (req, res) => {
+  const { colocationId } = req.params;
+  try {
+    const colocation = await models.colocation.findByPk(colocationId);
+    if (colocation) {
+      res.json({ colocation });
+    } else {
+      res.status(404).json({ error: "Colocation not found" });
+    }
+  } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
@@ -36,47 +41,12 @@ export const createColocation = async (req, res) => {
   }
 };
 
-export const getColocationById = async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const colocation = await models.colocation.findByPk(id, {
-      include: [
-        "users",
-        {
-          model: models.objective,
-          as: "objectives",
-          include: ["task", "outgoing"],
-        },
-        "admin_user",
-      ],
-    });
-
-    if (colocation) {
-      res.json({ colocation });
-    } else {
-      res.status(404).json({ error: "Colocation not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
-
 export const getColocationByAdmin = async (req, res) => {
   const { adminUserId } = req.params;
 
   try {
     const colocation = await models.colocation.findOne({
-      where: { admin_user_id: adminUserId },
-      include: [
-        "users",
-        {
-          model: models.objective,
-          as: "objectives",
-          include: ["task", "outgoing"],
-        },
-        "admin_user",
-      ],
+      where: { admin_user_id: adminUserId }
     });
 
     if (colocation) {
@@ -90,12 +60,12 @@ export const getColocationByAdmin = async (req, res) => {
 };
 
 export const updateColocationAdmin = async (req, res) => {
-  const { id } = req.params;
+  const { colocationId } = req.params;
   const { admin_user_id } = req.body;
 
   try {
-    const colocation = await models.colocation.findByPk(id);
-
+    const colocation = await models.colocation.findByPk(colocationId);
+    console.log(colocation)
     if (colocation) {
       await colocation.update({ admin_user_id });
       res.json({ colocation });
@@ -107,21 +77,55 @@ export const updateColocationAdmin = async (req, res) => {
   }
 };
 
+
 // A tester
 export const addColocationMember = async (req, res) => {
-  const colocation_id = req.params;
+  const { colocationId } = req.params;
   const { user_id } = req.body;
 
   try {
     const user = await models.user.findByPk(user_id);
 
     if (user) {
-      await user.update({ colocation_id });
-      res.json({ user });
+      // Si le USER est deja assigné a une Colocation , on la remplace directement ?
+      if (user.colocation_id !== colocationId) {
+        await user.update({
+          "colocation_id" : colocationId
+        });
+        res.json({ user });
+      } else {
+        res.json({ message: "User is already a member of the colocation." });
+      }
     } else {
-      res.status(404).json({ error: "Colocation not found" });
+      res.status(404).json({ error: "User not found" });
     }
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+
+export const deleteColocationMember = async (req, res) => {
+  const { colocationId } = req.params;
+  const { user_id } = req.body;
+  try {
+    const user = await models.user.findByPk(user_id);
+    const userData = user.dataValues;
+    
+    if (userData) {
+      if (userData.colocation_id && userData.colocation_id == colocationId) {
+        await user.update({ colocation_id: null });
+        res.json({ message: "User removed from the colocation." });
+      } else {
+        res.status(404).json({ error: "User is not a member of the colocation." });
+      }
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
